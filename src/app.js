346f2dc44988aaa046cb294993b7e61d5c08cd7c
James Hansen <enkindledesign@gmail.com>
2015-06-26 08:34:35 -0600

    All code created on first day

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
-import React from 'react'
-require('./styles/main.styl')
+import Router from './router'
+import styles from './styles/main.styl'
+import icons from 'octicons/octicons/octicons.css'
+import app from 'ampersand-app'
+import Me from './models/me'
 
-const Hello = React.createClass({
-	render : function(){
-		return <div>Hello, {this.props.name}</div>
-	}
+window.app = app
+
+app.extend({
+  init () {
+    this.me = new Me()
+    this.me.fetchInitialData()
+    this.router = new Router()
+    this.router.history.start()
+  }
 })
 
-React.render(<Hello name="James."/>, document.body)
+app.init()
